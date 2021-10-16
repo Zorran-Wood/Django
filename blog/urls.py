8ef4328fe5fe1b6529a6e2d0f121062dd1f0a4ee
Zorran Wood <63366766+Zorran-Wood@users.noreply.github.com>
2021-10-16 16:23:07 -0400

    Update urls.py
    added a comment to the camry url link, so i dont forget

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [ #routes for the urls
     path('' , views.home, name='blog-home'),#accessing the home method in views
     path('about/' , views.about, name='blog-about'),#accessing the about method in views
     path('cyclura/' , views.cyclura, name='blog-cyclura'),#accessing the cyclura method in views
-    path('camry/' , views.camry, name='blog-camry'),
+    path('camry/' , views.camry, name='blog-camry'),#accessing the camry method in views
 ]
